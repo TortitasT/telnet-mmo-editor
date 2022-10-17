12e8d8f3fca789b9203a09e9b63e8ccff669a584
Victor Garcia Fernandez <victorgf2011@gmail.com>
2022-10-17 16:58:00 +0200

    update

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,32 +1,71 @@
 <template>
   <div class="map">
-    <h2>Map</h2>
     <div class="map__wrapper">
       <table>
-        <tr v-for="(row , rowIndex) in map" :key="rowIndex">
-          <td v-for="(cell, cellIndex) in row" :key="cellIndex">
-            <div class="cell">
-              {{ cell }}
+        <tr v-for="(row , rowIndex) in map.tiles" :key="rowIndex">
+          <td v-for="(cell, cellIndex) in row" :key="cellIndex" @contextmenu.prevent="handleClick($event, item)">
+            <div class="cell" >
+              {{ cell.type }}
             </div>
           </td>
         </tr>
       </table>
     </div>
+    <vue-simple-context-menu
+      element-id="tileContextMenu"
+      :options="options"
+      ref="tileContextMenu"
+      @option-clicked="optionClicked"
+    />
   </div>
 </template>
 
 <script>
+import VueSimpleContextMenu from 'vue-simple-context-menu';
+
 export default {
   name: 'MapComponent',
+  components: {
+    VueSimpleContextMenu
+  },
   props: {
     map: {
-      type: Array,
+      type: Object,
       required: true
     }
-  }
+  },
+  data () {
+    return {
+      options: [
+        {
+          name: 'Edit',
+          value: 'edit'
+        },
+        {
+          name: 'Delete',
+          value: 'delete'
+        }
+      ]
+    }
+  },
+  methods: {
+    handleClick (event, item) {
+      this.$refs.tileContextMenu.showMenu(event, item)
+    },
+    optionClicked (event) {
+      // const target = JSON.parse(JSON.stringify(event))
+      const option = JSON.parse(JSON.stringify(event)).option
+      
+      console.log(event);
+      if (option.value === 'edit') {
+        alert('edit')
+      }
+    }
+  },
 }
 </script>
 
 <style lang="scss">
 @import "../assets/scss/map.scss";
+@import "../assets/scss/vue-simple-context-menu.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import VueSimpleContextMenu from 'vue-simple-context-menu';
+import 'vue-simple-context-menu/dist/vue-simple-context-menu.css';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
+
+app.component('vue-simple-context-menu', VueSimpleContextMenu);
